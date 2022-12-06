d5e9f9bfd8cdba3312b97796f5fa4cad454d4745
René Mayrhofer <rene@mayrhofer.eu.org>
2022-12-06 08:53:26 +0100

    Try to bundle together a few more libraries to make this work

--- emailbot.py
+++ emailbot.py
@@ -1,5 +1,10 @@
 from revChatGPT.revChatGPT import Chatbot
 import json
+import email
+import logging
+
+logger = logging.getLogger('chatgpt-email')
+logger.setLevel(logging.DEBUG)
 
 # example from https://github.com/acheong08/ChatGPT
 
@@ -7,19 +12,32 @@ import json
 configfile = f.open("config.json", "r")
 config = configfile.readlines()
 
+logger.debug("Using login data: " + config)
+
 promptfile = f.open("queryprompt.txt", "r")
 queryprompt = promptfile.readlines()
 
+logger.debug("Prompt query for priming ChatGPT session: " + queryprompt)
+
+newmsg = email.message_from_bytes(sys.stdin)
+
+logger.debug("Processing new incoming email message: " + newmsg.as_string())
+
+
+
+# TODO: use the email thread-id as conversation_id
 chatbot = Chatbot(config, conversation_id=None)
 chatbot.reset_chat() # Forgets conversation
 chatbot.refresh_session() # Uses the session_token to get a new bearer token
-resp = chatbot.get_chat_response(prompt, output="text") # Sends a request to the API and returns the response by OpenAI
-resp['message'] # The message sent by the response
-resp['conversation_id'] # The current conversation id
-resp['parent_id'] # The ID of the response
+resp = chatbot.get_chat_response(queryprompt, output="text") # Sends a request to the API and returns the response by OpenAI
+#resp['message'] # The message sent by the response
+#resp['conversation_id'] # The current conversation id
+#resp['parent_id'] # The ID of the response
 
- # This returns a stream of text (live update)
-resp = chatbot.get_chat_response(prompt, output="stream") 
-for line in resp: # You have to loop through the response stream
-        print(line['message']) # Same format as text return type
-        ...
+resp = chatbot.get_chat_response(newmsg.get_body('plain'), output="text") # Sends a request to the API and returns the response by OpenAI
+
+# # This returns a stream of text (live update)
+#resp = chatbot.get_chat_response(prompt, output="stream") 
+#for line in resp: # You have to loop through the response stream
+#        print(line['message']) # Same format as text return type
+#        ...
